e8274dd62c014b93dd8cbe35b1b5aae763146c1d
park <qkralstjd02@gmail.com>
2023-03-08 10:40:37 +0900

    2023-03-08 21. 데이터 유형 bool, char, string / 23. 연습문제

--- Section3/HelloWorld/app/src/main/java/kr/ilf/helloworld/KtlinBasics.kt
+++ Section3/HelloWorld/app/src/main/java/kr/ilf/helloworld/KtlinBasics.kt
@@ -1,6 +1,6 @@
 package kr.ilf.helloworld
 
-fun main(){
+fun main() {
     // immutable variable
     // TODO: Add new functionality
 
@@ -12,7 +12,8 @@ fun main(){
 
     // type string
     val myName = "Frank"
-    //type int 32bit
+    // type int 32bit
+    // type inference finds out the type from context
     var myAge = 28
 
     // Integer TYPES: Byte(8 bit), Short(16 bit)
@@ -26,5 +27,30 @@ fun main(){
     val myFloat: Float = 13.37F
     val myDouble: Double = 3.14159265458979323846
 
-    print("Hello" + myName)
+    // Boolean he type Boolean is used to represent logical values.
+    // It can have two possible values true and false.
+    var isSunny: Boolean = true
+    isSunny = false
+
+    // Characters
+    val letterChar = 'A'
+    val digitChar = '1'
+
+    // Strings
+    val myStr = "Hello World"
+    var firstCharInStr = myStr[0]
+    var lastCharIntStr = myStr[myStr.length - 1]
+
+
+    print("Last Character: " + lastCharIntStr)
+
+    // 23. 연습문제
+    var string: String = "Android Masterclass"
+    val float: Float = 13.37F
+    val pi: Double = 3.14159265358979
+    var byte: Byte = 25
+    var short: Short = 2020
+    var long: Long = 18881234567
+    var boolean: Boolean = true
+    var char: Char = 'a'
 }
