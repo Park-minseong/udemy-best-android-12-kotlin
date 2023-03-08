cd618b9e7f9d2f742aa26a15abc70facfd5638c2
park <qkralstjd02@gmail.com>
2023-03-08 10:18:07 +0900

    2023-03-08 20. 숫자데이터 유형 끝

---
+++ Section3/HelloWorld/app/src/main/java/kr/ilf/helloworld/KtlinBasics.kt
@@ -0,0 +1,30 @@
+package kr.ilf.helloworld
+
+fun main(){
+    // immutable variable
+    // TODO: Add new functionality
+
+    /*
+    This
+    is a multiline
+    comment
+    */
+
+    // type string
+    val myName = "Frank"
+    //type int 32bit
+    var myAge = 28
+
+    // Integer TYPES: Byte(8 bit), Short(16 bit)
+    // Int(32 bit), Long(64 bit)
+    val myByte: Byte = 13
+    val myShort: Short = 125
+    val myInt: Int = 123123123
+    val myLong: Long = 39_812_309_487_120_300
+
+    // Floating Point number Types: Float (32 bit), Double (64 bit)
+    val myFloat: Float = 13.37F
+    val myDouble: Double = 3.14159265458979323846
+
+    print("Hello" + myName)
+}
