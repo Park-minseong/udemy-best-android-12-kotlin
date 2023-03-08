5921262a2ddfccaa2ba465ebc3fb03d8ee444ab4
park <qkralstjd02@gmail.com>
2023-03-08 18:51:33 +0900

    2023-03-08 56. 변수 스코프와 쉐도잉

--- Section4/HelloWorld/app/src/main/java/kr/ilf/helloworld/KotlinBasics.kt
+++ Section4/HelloWorld/app/src/main/java/kr/ilf/helloworld/KotlinBasics.kt
@@ -9,6 +9,7 @@ fun main() {
     val mobilePhone3 = MobilePhone("IOS", "Apple", "IPhone 14 pro")
 }
 
+// 53. 클래스와 초기화
 class Person(firstName: String = "John", lastName: String) {
 
     //initializer Block
@@ -25,4 +26,9 @@ class MobilePhone(var osName: String, var brand: String, var model: String) {
     init {
         println("osBrand: $osName, brand: $brand, model: $model")
     }
+}
+
+// 56. 변수 영역과 쉐도잉
+fun myFunction(a: Int) {
+    var a = 3  //shadowing
 }
