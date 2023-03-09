c6e5936ffb78f3a17d8ec581761054f4e16a92ec
park <qkralstjd02@gmail.com>
2023-03-09 10:24:11 +0900

    2023-03-09 57. 멤버 변수 - 함수와 생성자

--- Section4/HelloWorld/app/src/main/java/kr/ilf/helloworld/KotlinBasics.kt
+++ Section4/HelloWorld/app/src/main/java/kr/ilf/helloworld/KotlinBasics.kt
@@ -1,23 +1,47 @@
 package kr.ilf.helloworld
 
 fun main() {
-    var denis = Person("Denis", "Panjuta")
-    var john = Person(lastName = "Peterson")
+    var denis = Person("Denis", "Panjuta", 31)
+    denis.hobby = "to skateboard"
+    denis.age = 32
+    println("Denis is ${denis.age} years old")
+    denis.stateHobby()
 
-    val mobilePhone1 = MobilePhone("Android", "Samsung", "Galaxy S22 Ultra")
-    val mobilePhone2 = MobilePhone("Android", "Samsung", "Galaxy S20 Ultra")
-    val mobilePhone3 = MobilePhone("IOS", "Apple", "IPhone 14 pro")
+    var john = Person(lastName = "Peterson")
+    john.hobby = "play video games"
+    john.stateHobby()
+//    val mobilePhone1 = MobilePhone("Android", "Samsung", "Galaxy S22 Ultra")
+//    val mobilePhone2 = MobilePhone("Android", "Samsung", "Galaxy S20 Ultra")
+//    val mobilePhone3 = MobilePhone("IOS", "Apple", "IPhone 14 pro")
 }
 
-// 53. 클래스와 초기화
+// 53. 클래스와 초기화, 57. 멤버변수 - 함수과 생성자
 class Person(firstName: String = "John", lastName: String) {
+    // Member Variables = Properties
+    var firstName : String? = null
+    var age : Int? = null
+    var hobby : String = "watch Netflix"
 
     //initializer Block
     init {
+        this.firstName = firstName
         println(
             "Initialized a new Person object with " +
                     "firstName = $firstName and lastName = $lastName"
         )
+    }
+
+    // Member secondary Constructor
+    constructor(firstName: String, lastName: String, age: Int) : this(firstName, lastName) {
+        this.age = age
+        println("Initialized a new Person object with " +
+                "firstName = $firstName and lastName = $lastName and age $age")
+    }
+
+
+    // Member Function = Method
+    fun stateHobby(){
+        println("$firstName hobby is $hobby")
     }
 }
 
